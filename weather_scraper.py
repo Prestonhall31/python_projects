f5a1d0d1938a8982e88439f51bd4f3b4873387a9
Preston Hall <preston@Prestons-MacBook-Pro.local>
2019-08-21 22:00:06 -0700

    Enclosed code into a function

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,64 +1,29 @@
+'''
+This code pull data from the weather.com website and stores the current weather data in a dicstionary
+'''
+
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://weather.com/en-IN/weather/today/l/97003:4:US")
 
-soup = BeautifulSoup(page.content,"html.parser")
+def current_weather():
+    page = requests.get("https://weather.com/en-IN/weather/today/l/97003:4:US")
 
-# Print all of the data from the Right Now field. 
-right_now = soup.find("div",{"class":"today_nowcard-sidecar component panel"})
+    soup = BeautifulSoup(page.content,"html.parser")
 
-data = {}
-for span_tag in right_now.find_all('th'):
-    data[span_tag.text] = span_tag.next_sibling.text
-    # print(span_tag.text, span_tag.next_sibling.text)
+    # Print all of the data from the Right Now field. 
+    right_now = soup.find("div",{"class":"today_nowcard-sidecar component panel"})
 
-print(data)
-'''
-<div class="today_nowcard-sidecar component panel">
-    <table>
-        <caption>Right Now</caption>
-        <tbody>
-            <tr>
-                <th>Wind</th>
-                <td>
-                    <span class="">WSW 10 km/h </span>
-                </td>
-            </tr>
-            <tr>
-                <th>Humidity</th>
-                <td>
-                    <span class="">
-                        <span>63
-                            <span class="Percentage__percentSymbol__2Q_AR">%</span>
-                        </span>
-                    </span>
-                </td>
-            </tr>
-            <tr>
-            <th>Dew Point</th>
-                <td>
-                    <span class="">12
-                        <sup>°</sup>
-                    </span>
-                </td>
-            </tr>
-            <tr>
-                <th>Pressure</th>
-                <td>
-                    <span class="">1,015.9 mb<!-- --> 
-                        <span class="icon icon-font iconset-arrows icon-arrow-up-line" classname="icon icon-font iconset-arrows icon-arrow-up-line">
-                        </span>
-                    </span>
-                </td>
-            </tr>
-            <tr>
-                <th>Visibility</th>
-                <td>
-                    <span class="">16.1 km</span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-'''
+    # Store data in a dictionary 
+    data = {}
+    for span_tag in right_now.find_all('th'):
+        data[span_tag.text] = span_tag.next_sibling.text
+
+    return data
+
+    # for k, v in data.items():
+    #     print(k, ": ", v)
+
+
+if __name__ == "__main__":
+    current_weather()
